6de66c9f871676ad74114b440d1b8a8c0cfe2eec
Chioma Okeke <omaokeke28@gmail.com>
2021-01-10 14:12:42 +0100

    changes

--- models/db.config.js
+++ models/db.config.js
@@ -1,18 +1,18 @@
 var mysql = require('mysql')
 /****************************************** Database Connection **************************************/
 require('dotenv').config();
-var connection = mysql.createConnection({
+var connection = mysql.createPool({
     "host" : process.env.DB_HOST,
     "user" : process.env.DB_USER,
-    "port" : process.env.DB_PORT,
+   // "port" : process.env.DB_PORT,
     "password" : process.env.DB_PASSWORD,
     "database" : process.env.DB_DATABASE
 });
-connection.connect((err,res)=>{
-    if(err) throw err
+// connection.((err,res)=>{
+//     if(err) throw err
 
-    console.log('db server connected')
-});
+//     console.log('db server connected')
+// });
  
  
 module.exports = connection;
